1b2f9d3d3315fe78ddc057aa7394be8603cd4788
Learner <32327971+Learner045@users.noreply.github.com>
2017-10-17 22:14:40 +0530

    Update HashTable.java

--- HashTable.java
+++ HashTable.java
@@ -11,8 +11,17 @@ public class HashTable {
    public void put(int key,int value){
        int index=hashFunction(key);
 
-       while(hashTable[index]!=null){
+       //if hashtable at index is not null that means it has data and
+       //we also check if this data's key is not equal to the key we are trying to newly put
+       //it will break prematurely if it finds a key
+       //then we only need to update the key
+       //if it becomes null when it finds an empty spot then we add new data at that index
+       while(hashTable[index]!=null && hashTable[index].getKey()!=key){
            index=(index+1)%10;
+       }
+       if(hashTable[index]!=null && hashTable[index].getKey()==key){
+           //key already present so update values
+           hashTable[index].setValue(value);return;
        }
        hashTable[index]=new HashItem(key,value);
 
